ef205883999d95bc54a2640b9e24f8d7fba56e0c
gnoul_ <ttluong4869@gmail.com>
2021-10-29 00:50:19 +0700

    Install ESlint

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -50,7 +50,7 @@ const ProductScreen = () => {
                                         {product.countInStock > 0 ? (
                                             <span className="success">In Stock</span>
                                         ) : (
-                                            <span className="danger">Unavailable</span>
+                                            <span className="danger ">Unavailable</span>
                                         )}
                                     </div>
                                 </div>
