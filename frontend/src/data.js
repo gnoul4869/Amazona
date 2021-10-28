2550c87824f930e0c930379615d5d0c8f8faa269
gnoul_ <ttluong4869@gmail.com>
2021-10-28 11:53:51 +0700

    Install react frontend and add template files to react with data

---
+++ frontend/src/data.js
@@ -0,0 +1,78 @@
+const data = {
+    products: [
+        {
+            id: 0,
+            name: 'Regular Fit Suit Jacket', // Pierre Cardin Navy Twill Regular Fit Suit Jacket
+            category: 'Suit Jacket',
+            image: './images/pierre_cardin_navy_twill_regular_fit_suit_jacket.jpg',
+            price: 50,
+            brand: 'Pierre Cardin',
+            rating: 4.5,
+            numReviews: 10,
+            description:
+                'Look smart and stylish in the classic Pierre Cardin range. All suits are made in a regular fit for comfort, and feature a 2 button jacket with notch lapel. We also offer a range of ties and shirts to complement your new suit.',
+        },
+        {
+            id: 1,
+            name: 'Double Face Shirt', // Karlskrona Double Face Shirt
+            category: 'Casual Shirt',
+            image: './images/melka_karlskrona_double_face_shirt.jpg',
+            price: 25,
+            brand: 'Melka',
+            rating: 5,
+            numReviews: 10,
+            description:
+                'A double cloth shirt to transform your wardrobe - Melka style. introducing the unique Karlskrona, a lightweight cotton shirt with a difference, with long sleeves, a patch chest pocket and a button down, classic collar inspired by our traditional Oxford styling.',
+        },
+        {
+            id: 2,
+            name: 'Navy Blazer', // Scott By The Label Navy Blazer
+            category: 'Blazer',
+            image: './images/scott_by_the_label_navy_blazer.jpg',
+            price: 145,
+            brand: 'Scott By The Label',
+            rating: 4,
+            numReviews: 10,
+            description:
+                "A stylish men's wool blend blazer in navy blue from Scott by the Label, with a versatile, classic fit.",
+        },
+        {
+            id: 3,
+            name: 'Charcoal Melton Overcoat', // Racing Green Charcoal Melton Overcoat
+            category: 'Coat',
+            image: './images/racing_green_charcoal_melton_overcoat.jpg',
+            price: 95,
+            brand: 'Racing Green',
+            rating: 5,
+            numReviews: 10,
+            description:
+                "This dark grey, wool blend overcoat is a unique way to style any wardrobe - a top coat for men inspired by classic British tailoring. With notch lapels, a single breasted three buton fastening, a tailored fit and plenty of pocket space, this men's wool blend jacket is ideal for any wardrobe.",
+        },
+        {
+            id: 4,
+            name: 'V Neck Jumper', // Jett Banks V Neck Jumper
+            category: 'Suit Jacket',
+            image: './images/jeff_banks_v_neck_jumper.jpg',
+            price: 40,
+            brand: 'Jett Banks',
+            rating: 4.5,
+            numReviews: 10,
+            description:
+                'Carefully crafted, this V-neck jumper is made from pure cotton for a luxurious, soft-to-touch feel. With classic metal plaque branding to the side, this jumper is designed for comfort and durability.',
+        },
+        {
+            id: 5,
+            name: 'Plain Blue Panama Slim Fit Suit', // Lemehaus Plain Blue Panama Slim Fit Suit
+            category: 'Suit Jacket',
+            image: './images/limehaus_plain_blue_panama_slim_fit_jacket.jpg',
+            price: 125,
+            brand: 'Lemehaus',
+            rating: 5,
+            numReviews: 10,
+            description:
+                "Limehaus are a true trendsetter in menswear, with statement suits that are cut slimmer than usual. We'd recommend sizing up to get the perfect fit.",
+        },
+    ],
+};
+
+export default data;
