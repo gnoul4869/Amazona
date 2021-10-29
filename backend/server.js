cf386e978171072c80aea0863e4084249b06e8b4
gnoul_ <ttluong4869@gmail.com>
2021-10-29 15:10:16 +0700

    Add redux to product screen

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,15 @@ app.get('/api/products', (req, res) => {
     res.send(data.products);
 });
 
+app.get('/api/products/:id', (req, res) => {
+    const product = data.products.find((item) => item.id === req.params.id);
+    if (product) {
+        res.send(product);
+    } else {
+        res.status(404).send({ message: 'Product not Found' });
+    }
+});
+
 app.get('/', (req, res) => {
     res.send('Server is ready');
 });
