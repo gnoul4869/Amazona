ced88eaefb72b9bd450a0c1fe3c250d06f8ac1cb
gnoul_ <ttluong4869@gmail.com>
2021-11-07 14:17:31 +0700

    Create SigninScreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 import CartScreen from './screens/CartScreen';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import SigninScreen from './screens/SigninScreen';
 
 function App() {
     const cart = useSelector((state) => state.cart);
@@ -30,6 +31,7 @@ function App() {
                 <main>
                     <Route path="/cart/:id?" component={CartScreen}></Route>
                     <Route path="/product/:id" component={ProductScreen}></Route>
+                    <Route path="/signin" component={SigninScreen}></Route>
                     <Route exact path="/" component={HomeScreen}></Route>
                 </main>
                 <footer className="row center">All right reserved</footer>
